2966864d372111c4250c87b89f0ce3acfdcd4483
hanseul <w0w1278@naver.com>
2022-03-16 19:01:26 +0900

    input 수정중

--- law_final.py
+++ law_final.py
@@ -1,8 +1,7 @@
 import sys
 from newform import Ui_Form
 from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QMainWindow
-from PyQt5.QtGui import QDesktopServices
-from PyQt5.QtCore import QUrl
+
 
 # import json
 #
@@ -28,23 +27,44 @@ class Main(QMainWindow,Ui_Form):
 
     def set_slot(self):
         self.search_btn.clicked.connect(self.search_laws)
+        self.search_law.textChanged.connect(self.input_text)
 
-    # def link(self, linkStr):
-    #     QDesktopServices.openUrl(QUrl(linkStr))
+    def input_text(self):
+        self.input_text_data = self.search_law.text()
+        pass
+
 
     # btn이 눌리면 작동할 함수
     def search_laws(self):
         # linkTemplate ='<a href={0}>{1}</a>'
         linkTemplate = '<a href=\"https://www.law.go.kr/DRF/lawService.do?OC=w0w1278&target=law&MST={0}&type=HTML\">{1}</a>'
 
+        p = self.input_text_data
+        s = "query2"
+        d = "query3"
+        f = "query4"
+        c = "query5"
+
+        number1 = str(3)
+
+        # tatal_laws_data.iloc[i]["법령명"])
+        # tatal_laws_data.iloc[i]["법령MST"])
+        # numbertatal_laws_data.iloc[i]["조문번호"]
+
+        #query1 = list(result_law_dic.keys())[0] key값 가져오기
+
+
         # self.law1.setOpenExternalLinks(True)
         # self.law1.linkActivated.connect(self.link)
-        self.law1.setText(linkTemplate.format("210636","query1"))
+        self.law1.setText(linkTemplate.format("210636",p) + "\t "+ number1 +"조")
         #self.law1.setTextInteractionFlags(TextSelectableByMouse)
-        self.law2.setText(linkTemplate.format("240223","query2"))
-        self.law3.setText(linkTemplate.format("224453","query3"))
-        self.law4.setText(linkTemplate.format("224453","query4"))
-        self.law5.setText(linkTemplate.format("224453","query5"))
+        self.law2.setText(linkTemplate.format("240223",s ))
+
+        self.law3.setText(linkTemplate.format("224453",d ))
+
+        self.law4.setText(linkTemplate.format("224453",f ))
+
+        self.law5.setText(linkTemplate.format("224453",c ))
 
     def Urls(self):
         pass
